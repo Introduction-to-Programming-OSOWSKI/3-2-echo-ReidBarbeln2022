e45db46f9581c40302a77a35cc31bf126a9ac1ff
ReidBarbeln2022 <814684@mystma.org>
2021-01-20 16:02:31 +0000

    echo try 1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+def echo(x, y):
+    
+    total = x
+    
+    
+    for i in range (0, y - 1):
+        total = total + x
+
+
+    return total
+
+print (echo("hello", 5))
+
